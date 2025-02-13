7eb1267299709cf98913091ea1b4f789aed503b7
Filipe Fernandes <ocefpaf@gmail.com>
2025-02-13 19:25:54 +0100

    clean unused stuff

--- copy_to_site.sh
+++
@@ -1,8 +0,0 @@
-# Need to install watchdog:
-#!/bin/bash
-
-watchmedo shell-command \
-          --patterns="*.py;*.rst" \
-          --ignore-pattern='_build/*' \
-          --recursive \
-          --command='make json && cp -r _build/json/* ${ATN_SITE}'
